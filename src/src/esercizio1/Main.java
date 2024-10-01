6d2610f94ef94ec228c87aa3beaac9a8df0de4bc
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-10-01 18:16:05 +0200

    1

--- src/src/esercizio1/Main.java
+++ src/src/esercizio1/Main.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args){
-ArrayList<String> arrayParoleScartate =  new ArrayList<String>();
-HashSet<String> arrayparole = new HashSet<String>();
+ArrayList<String> listaParoleScartate =  new ArrayList<String>();
+HashSet<String> setparole = new HashSet<String>();
      Scanner scanner = new Scanner(System.in);
      try {
              System.out.println("inserisci un numero degli elementi da inserire");
@@ -17,15 +17,12 @@ HashSet<String> arrayparole = new HashSet<String>();
         for ( int i = 0; i< numeroElementi ; i++){
             System.out.println("inserisci la parola numero "+ (i+1) );
             String parola = scanner.nextLine();
-            if(arrayparole.contains(parola)) {
+            if(!setparole.add(parola)) {
                 System.out.println("questa parola è già presente");
-                arrayParoleScartate.add(parola);
+                listaParoleScartate.add(parola);
             }
-            else {
-                arrayparole.add(parola);
-            }
-            System.out.println("le parole inserite sono : " + arrayparole);
-            System.out.println("le parole scartate in quanti duplicati sono : " + arrayParoleScartate);
+            System.out.println("le parole inserite sono : " + setparole);
+            System.out.println("le parole scartate in quanti duplicati sono : " + listaParoleScartate);
 
         }
 
